27afd8a2df3481b627b48af67c69276167e3ed32
Hari Solanki <harisolanki1804@gmail,com>
2024-07-19 13:45:01 +0530

    Updated About Us Page...

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// 1. Hamburger Menu
+
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.navLinks');
 const icon = document.querySelector('i.fa-solid');
@@ -11,3 +13,56 @@ hamburger.addEventListener('click', () => {
         icon.classList.replace('fa-xmark', 'fa-bars');
     }
 })
+
+
+// 2. Image Carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+
+// 3. Read More
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
